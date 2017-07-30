6f8e701fd317e865b0e79bdceaf942bdc2a0a03f
yanniks <yannik@yanniks.de>
2017-07-30 10:55:48 +0200

    Throw error if exists

--- parseLectureSchedule/parseLectures.js
+++ parseLectureSchedule/parseLectures.js
@@ -10,6 +10,7 @@ this.parseLectures = function(course, callback) {
 				fs.rename(filename, 'tmp/' + filename, function (err) {
 					exec("java -Dfile.encoding=UTF-8 -jar java/tabula-0.9.2-jar-with-dependencies.jar -g -n -o tmp/" + filename + ".csv tmp/" + filename, function(error, stdout, stderr) {
 						fs.readFile( 'tmp/' + filename + '.csv', function (err, data) {
+							if (err) throw err
 							gd.getDates(course, data.toString(), function(schedule) {
 								callback(schedule);
 							})
