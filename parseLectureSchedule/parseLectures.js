1bd4a99be716925f8181e9f09a64d028112a8d74
yanniks <yannik@yanniks.de>
2017-07-30 12:30:08 +0200

    Change fs.rename to mv

--- parseLectureSchedule/parseLectures.js
+++ parseLectureSchedule/parseLectures.js
@@ -1,4 +1,4 @@
-var fd = require('./fileDownload.js'), fc = require('./fileComparison.js'), fs = require('fs'), gd = require('./getDates.js'), exec = require('child_process').exec;
+var fd = require('./fileDownload.js'), fc = require('./fileComparison.js'), fs = require('fs'), gd = require('./getDates.js'), exec = require('child_process').exec, mv = require('mv');
 var courses = { wwi2016a: "http://studium.dhbw-stuttgart.de/fileadmin/wi/Vorlesungsplaene_WI/Vorlesungsplaene_Wenger/Vorlesungsplan_2016A_3Sem.pdf", wwi2015a: "http://studium.dhbw-stuttgart.de/fileadmin/wi/Vorlesungsplaene_WI/Vorlesungsplaene_Wenger/Vorlesungsplan_WWI2015A_4Sem.pdf" };
 
 this.parseLectures = function(course, callback) {
@@ -7,7 +7,7 @@ this.parseLectures = function(course, callback) {
 			if (!result) {
 				try {fs.unlinkSync('tmp/' + filename + '.csv');} catch (err) {}
 				try {fs.unlinkSync('tmp/' + filename);} catch (err) {}
-				fs.rename(filename, 'tmp/' + filename, function (err) {
+				mv(filename, 'tmp/' + filename, function (err) {
 					if (err) throw err;
 					exec("java -Dfile.encoding=UTF-8 -jar java/tabula-0.9.2-jar-with-dependencies.jar -g -n -o tmp/" + filename + ".csv tmp/" + filename, function(error, stdout, stderr) {
 						if (error) throw error;
