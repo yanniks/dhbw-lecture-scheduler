d5c64a63ce18d160cac4950864a593cf4357dde0
Yannik Ehlert <kontakt@yanniks.de>
2018-02-15 14:46:28 +0100

    DHBW moved on to HTTPS

--- parseLectureSchedule/fileDownload.js
+++ parseLectureSchedule/fileDownload.js
@@ -1,11 +1,11 @@
-var http = require('http');
+var https = require('https');
 var fs = require('fs');
 
 this.downloadFile = function(url, callback) {
 	var urlSplit = url.split("/");
 	var filename = urlSplit[urlSplit.length - 1];
 	var file = fs.createWriteStream('tmp/new.' + filename);
-	var request = http.get(url, function(response) {
+	var request = https.get(url, function(response) {
 		response.pipe(file);
 		callback(filename);
 	});
