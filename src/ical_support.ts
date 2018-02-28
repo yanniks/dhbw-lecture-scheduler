c1b7d107a7bb4ebe09c6ef05a38579ecf55896d3
Yannik Ehlert <kontakt@yanniks.de>
2018-02-28 17:53:04 +0100

    (Proper) support for whole-day events

--- src/ical_support.ts
+++ src/ical_support.ts
@@ -5,8 +5,16 @@ export function generateIcal(lectures: any, courseTitle: string, res: any) {
     lectures.forEach((event) => {
         ical += "BEGIN:VEVENT\n";
         ical += "SUMMARY:" + event.title + "\n";
-        ical += "DTSTART:" + event.begin.replace(/-/g, "").replace(/:/g, "") + "\n";
-        ical += "DTEND:" + event.end.replace(/-/g, "").replace(/:/g, "") + "\n";
+        const begin = event.begin.replace(/-/g, "").replace(/:/g, "") + "\n";
+        const end = event.end.replace(/-/g, "").replace(/:/g, "") + "\n";
+        if (begin.substring(8, 16) === "T220000Z" && end.substring(8, 16) === "T220000Z") {
+            // Seems like a whole-day event
+            ical += "DTSTART;VALUE=DATE:" + begin.substring(0, 8) + "\n";
+            ical += "DTEND;VALUE=DATE:" + begin.substring(0, 8) + "\n";
+        } else {
+            ical += "DTSTART:" + begin;
+            ical += "DTEND:" + end;
+        }
         ical += "LOCATION:" + event.location + "\n";
         if (event.prof) {
             ical += "ORGANIZER;CN=\"" + event.prof + "\"\n";
